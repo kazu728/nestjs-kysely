111f5da3068d960949d1b95b0929406dfab6cec3
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-03-15 10:43:33 +0900

    format

--- example/src/app.controller.ts
+++ example/src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { InjectKysely } from 'nestjs-kysely';
-import { DB } from './@types';
+import { Controller, Get } from "@nestjs/common";
+import { InjectKysely } from "nestjs-kysely";
+import { DB } from "./@types";
 
 @Controller()
 export class AppController {
@@ -9,8 +9,8 @@ export class AppController {
   @Get()
   async getHello(): Promise<string> {
     const result = await this.db
-      .selectFrom('person')
-      .innerJoin('pet', 'pet.owner_id', 'person.id')
+      .selectFrom("person")
+      .innerJoin("pet", "pet.owner_id", "person.id")
       .selectAll()
       .execute();
 
--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MysqlDialect } from 'kysely';
-import { createPool } from 'mysql2';
-import { KyselyModule } from 'nestjs-kysely';
-import { AppController } from './app.controller';
+import { Module } from "@nestjs/common";
+import { MysqlDialect } from "kysely";
+import { createPool } from "mysql2";
+import { KyselyModule } from "nestjs-kysely";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [
     KyselyModule.forRoot({
       dialect: new MysqlDialect({
         pool: createPool({
-          host: '127.0.0.1',
-          user: 'root',
-          password: 'password',
-          database: 'kysely_test',
+          host: "127.0.0.1",
+          user: "root",
+          password: "password",
+          database: "kysely_test",
         }),
       }),
     }),
